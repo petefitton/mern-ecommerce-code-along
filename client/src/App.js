8d6c912cb866f9a8dc7690a8267cc48559ab5906
Pete Fitton <petefitton@gmail.com>
2025-07-01 13:06:17 -0600

    Show Clients on frontend in a table with a loading spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import Header from './components/Header.jsx';
+import Clients from './components/Clients.jsx';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-    <Header />
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Hello World</h1>
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
