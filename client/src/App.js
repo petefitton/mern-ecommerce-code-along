97f19d2470fec9090f88d35ea8cc345392a8c582
Pete Fitton <petefitton@gmail.com>
2025-07-01 13:27:43 -0600

    Add delete client functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from './components/Header.jsx';
 import Clients from './components/Clients.jsx';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:5001/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
