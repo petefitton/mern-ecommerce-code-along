315404ca131b2c009851baf04216386fd30ebbe7
Pete Fitton <petefitton@gmail.com>
2025-07-01 18:04:36 -0600

    Create add project functionality on frontend

---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+const ADD_PROJECT = gql`
+  mutation addProject($name: String!, $description: String!, $status: ProjectStatus!, $clientId: ID!) {
+    addProject(name: $name, description: $description, status: $status, clientId: $clientId) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+// const DELETE_PROJECT = gql`
+//   mutation deleteProject($id: ID!) {
+//     deleteProject(id: $id) {
+//       id
+//       name
+//       email
+//       phone
+//     }
+//   }
+// `;
+
+export { ADD_PROJECT };
+// export { ADD_PROJECT, DELETE_PROJECT };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Clients from '../components/Clients.jsx';
 import Projects from '../components/Projects.jsx';
 import AddClientModal from '../components/AddClientModal.jsx';
+import AddProjectModal from '../components/AddProjectModal.jsx';
 
 export default function Home() {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
       </div>
       <Projects />
       <hr />
